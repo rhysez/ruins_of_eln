47ad00e1f41644117da41658a17d03a474dfba8a
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2025-01-04 19:49:58 +0000

    add colored crate

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,4 +1,5 @@
 use lib::char::{self, pc::PlayerChar};
+use colored::Colorize;
 
 fn main() {
     let new_pc: PlayerChar = PlayerChar {
@@ -11,5 +12,9 @@ fn main() {
         mental_state: String::from("Healthy")
     };
 
-    println!("Ruins of Eln alpha: Player constructed as {}", new_pc.name);
+    println!("--------------------------------------------------------");
+    println!("{}", "Ruins of Eln v0.1.0".bright_green().on_blue());
+    println!("--------------------------------------------------------");
+
+    println!("Player constructed as {}, {}, level {}", new_pc.name, new_pc.class, new_pc.level);
 }
